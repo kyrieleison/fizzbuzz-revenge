81ee1319d90035eac7503cd52768036f2c657ad1
Yuka Sato <yuka.sato@gaiax.com>
2017-05-30 12:03:54 +0900

    run fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,20 @@
 class Fizzbuzz
   def self.run
     puts "整数を入力してください\n"
+
+    input = gets.chomp
+
+    unless input =~ /\A[1-9][0-9]*\z/
+      puts '整数ではありません'
+      return false
+    end
+
+    if input.to_i % 3 === 0
+      puts 'Fizz'
+    elsif input.to_i % 5 === 0
+      puts 'Buzz'
+    else
+      puts input
+    end
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,11 +4,48 @@ describe Fizzbuzz do
   let(:output) { $stdout.string }
 
   before do
+    $stdin = StringIO.new(input)
     $stdout = StringIO.new
   end
 
-  it do
-    Fizzbuzz.run
-    expect(output).to eq("整数を入力してください\n")
+  after do
+    $stdin = STDIN
+    $stdout = STDOUT
+  end
+
+  context '3の倍数の場合' do
+    let(:input) { '3' }
+
+    it do
+      Fizzbuzz.run
+      expect(output).to eq("整数を入力してください\nFizz\n")
+    end
+  end
+
+  context '5の倍数の場合' do
+    let(:input) { '5' }
+
+    it do
+      Fizzbuzz.run
+      expect(output).to eq("整数を入力してください\nBuzz\n")
+    end
+  end
+
+  context 'それ以外の整数の場合' do
+    let(:input) { '1' }
+
+    it do
+      Fizzbuzz.run
+      expect(output).to eq("整数を入力してください\n1\n")
+    end
+  end
+
+  context '整数でない場合' do
+    let(:input) { 'NOT_INTEGER' }
+
+    it do
+      Fizzbuzz.run
+      expect(output).to eq("整数を入力してください\n整数ではありません\n")
+    end
   end
 end
